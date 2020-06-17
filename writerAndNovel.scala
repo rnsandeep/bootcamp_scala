41f5e9e0574ce91d59fbd6608fae7bb60f309c6e
Naga Sandeep Ramachandruni <nagasandeep.ramachandruni@avalara.com>
2020-06-17 22:22:07 +0530

    Update code for novel and writer

--- writerAndNovel.scala
+++ writerAndNovel.scala
@@ -6,5 +6,14 @@ class Writer(firstname:String, surname: String, val year: Int){
 
 class Novel(name:String, year_of_release: Int, author: Writer){
   def authorAge = year_of_release - author.year
+  def isWrittenBy(author: Writer) = author == this.author
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+}
+object Basics extends App {
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
 
+  println(novel.authorAge)
+  println(novel.isWrittenBy(imposter))
 }
